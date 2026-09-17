e1ecf73ab03a45f8d9515f7bd718582832c25dc7
Hampus Ulander <ulanderhampus1@gmail.com>
2026-09-17 13:56:09 +0200

    Kör databasmigrationer vid start

--- Redcode/Program.cs
+++ Redcode/Program.cs
@@ -52,6 +52,12 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
