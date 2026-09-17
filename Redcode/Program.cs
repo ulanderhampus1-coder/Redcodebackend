70390d0df5209c8e1fe6281ddc7f75878b3b7c76
Hampus Ulander <ulanderhampus1@gmail.com>
2026-09-17 13:32:25 +0200

    Tillåt frontend i Render CORS

--- Redcode/Program.cs
+++ Redcode/Program.cs
@@ -39,9 +39,12 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy.WithOrigins(
+    "http://localhost:4200",
+    "https://redcode-jdhs.onrender.com"
+)
+.AllowAnyHeader()
+.AllowAnyMethod();
     });
 });
 
